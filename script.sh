fa8cbe4be0698f0e5bd2797f3f6fe6a144bb20bf
paulohenriqueav <paulohenriqueav@gmail.com>
2017-04-18 11:33:38 -0300

    Create script.sh
    Script que mostra ao ADMIN quantidade de usuaários Simultâneos logados no ambiente Linux/Unix e Quantidade de Conexões Simultãneas

---
+++ script.sh
@@ -0,0 +1,29 @@
+########################################################################
+#                                                                      #
+#   Arquivo: relatorio Conexões                    Data: 18/04/2017    #
+#   Paulo Henrique de Andrade Vaz - Administrador de Redes             #
+#             Relatório de Conexões de usuários na HPUX                #
+#                                                                      #
+#                                                                      #
+#                                                                      #
+#                                                                      #
+#   Objetivo: Número Total de Usuários Simultâneos                     #
+#             Número de Usuários de Conexão                            #
+#             Número de Conexões por Usuários                          #
+#                                                                      #
+########################################################################
+
+USER_SIMUL=$(who -R | wc -l)    # Número Total de Usuários Simultâneos
+USER_CONEX=$(who -R | awk {'print $1'} | sort -u | wc -l)  # Número de Usuários de Conexão                          
+who -R | awk {'print $1'} | sort -u > user_conex.txt
+
+date
+while read i; do
+  user_conex_simult=$(who -R | awk {'print $1'} | grep $i | wc -l)
+  echo 'Usuario: ' $i '->' $user_conex_simult 'Conexao(oes)' 
+done < user_conex.txt
+
+rm -rf user_conex.txt 
+
+echo 'Numero Total de Usuarios Simultaneos: ' $USER_SIMUL
+echo 'Numero de Usuarios de Conexoes: ' $USER_CONEX 
